4c748b99ad0600b90f69449ea887aff8c84ec595
mirkotalentw <mirko.kalezic@talentwunder.com>
2024-07-22 09:19:38 +0200

    Updates the instruction and adds languages

--- app.py
+++ app.py
@@ -22,17 +22,19 @@ OpenAI.api_key = openai_api_key
 client = OpenAI()
 
 system_instruction = """
-You are a recruiter for a tech company. You are writing a job description for a new position.
+You are a recruiter for a tech company. You are writing a job description for a new position. The job description must be written on {APPLICATION_LANGUAGE}.
 The job title will be provided by the user. Please write a job description based on the job title with following requirements:
 The main duties and responsibilities should be described unambiguously and in detail. According to a StepStone survey, only 42 per cent of candidates feel that job descriptions outline duties and responsibilities in a way that gives a clear picture of the role.
 Describe the role in as much detail as possible:
 Use keywords to describe the role in five to eight bullet points
 Establish a hierarchy of duties by starting with the most important ones and ending with more minor details
 Round off the section with information on strategic topics, the level of operational responsibility and/or current projects
+The job description should be written in a way that is easy to understand and free of jargon. It should be tailored to the target group and should not contain any unnecessary information. Also, should not be too long or too short (it should contain in range 200-250 words).
+It should NOT CONTAINT the summary at the end.
 """
 
 system_instruction_2 = """
-You are a recruiter for a tech company. You are writing a requirement profile for a new position.
+You are a recruiter for a tech company. You are writing a requirement profile for a new position. The requirement profile must be written on {APPLICATION_LANGUAGE}.
 The job title will be provided by the user. Please write a requirements based on the job title with following requirements:
 The required specialist knowledge and the desired qualifications (vocational or academic) must be clearly stated in the requirements profile.
 At the very least, you should describe the qualification(s) needed and the specialist knowledge relevant to the position
@@ -41,6 +43,8 @@ If no qualifications or prior knowledge are required, make this clear
 Clearly state which requirements are essential and which are merely desirable
 Once again, it is important to avoid the usual buzzwords such as ‘team-oriented’, ‘communicative’ and ‘able to work under pressure’. There are very few positions for which these skills are not required. Instead, you should try to define – as a team, if possible – which specific skills are genuinely important for the vacancy in question.
 Always remember that applicants will be discouraged if the requirements are too high or too low. It’s all about striking the right balance.
+The requirements should be written in a way that is easy to understand and free of jargon. It should be tailored to the target group and should not contain any unnecessary information. Also, should not be too long or too short (it should contain around 200 words).
+It should NOT CONTAINT the summary at the end. 
 """
 
 
@@ -73,7 +77,15 @@ def display_login_form():
 def display_main_app():
     st.title('AI Job Description Generator')
     selected_model = "gpt-4o"
+    selected_lang = st.selectbox(
+    "Application language:",
+    ("en", "de"))
     user_input = st.text_input("Enter your job title:")
+    if selected_lang == "de":
+        language = "German"
+    else:
+        language = "English"
+
  
     if st.button('Generate Job Description'):
         if user_input:
@@ -82,7 +94,7 @@ def display_main_app():
                   model='gpt-4o',
                   temperature=0,
                   messages=[
-                    {"role": "system", "content": system_instruction},
+                    {"role": "system", "content": system_instruction.replace("{APPLICATION_LANGUAGE}", language)},
                     {"role": "user", "content": user_input},
                 ])
     
@@ -96,7 +108,7 @@ def display_main_app():
                   model='gpt-4o',
                   temperature=0,
                   messages=[
-                    {"role": "system", "content": system_instruction_2},
+                    {"role": "system", "content": system_instruction_2.replace("{APPLICATION_LANGUAGE}", language)},
                     {"role": "user", "content": user_input},
                 ])
     
